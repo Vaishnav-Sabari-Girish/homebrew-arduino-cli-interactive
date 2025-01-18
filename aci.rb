bc97bdf98a01584714022d123a0c994994d3772a
vaishnav_sabari_girish <forgamesonly684@gmail.com>
2025-01-18 11:31:08 +0530

    V1.0.6

--- aci.rb
+++ aci.rb
@@ -2,7 +2,7 @@ class Aci < Formula
   desc "Interactive Arduino-CLI for beginners transitioning from Arduino IDE"
   homepage "https://github.com/Vaishnav-Sabari-Girish/arduino-cli-interactive"
   url "https://github.com/Vaishnav-Sabari-Girish/arduino-cli-interactive/releases/download/v1.0.6/aci_v1.0.6.tar.gz"
-  sha256 "036f9c7e79bb8d7f1ac80d917c51efc96a5f220405971633b62d78a99cb43e44"
+  sha256 "7b5adb2d6f7cd1fd12e34c509a0525046a6136c1f9975d86b099fe698f30520c"
   license "Apache-2.0"
 
 
