dfd26dfbf7180da88444d0549fd4b61a3fcd79c9
vaishnav_sabari_girish <forgamesonly684@gmail.com>
2025-02-15 19:06:33 +0530

    fix(file_empty_bug): When a file was opened, the file used to be empty

--- aci.rb
+++ aci.rb
@@ -1,8 +1,8 @@
 class Aci < Formula
   desc "Interactive Arduino-CLI for beginners transitioning from Arduino IDE"
   homepage "https://github.com/Vaishnav-Sabari-Girish/arduino-cli-interactive"
-  url "https://github.com/Vaishnav-Sabari-Girish/arduino-cli-interactive/releases/download/v1.0.8/aci_v1.0.8.tar.gz"
-  sha256 "e7ca759e7030266531cff7806db463941bd79de98834d78581a05cf76312b4f0"
+  url "https://github.com/Vaishnav-Sabari-Girish/arduino-cli-interactive/releases/download/v1.0.9/aci_v1.0.9.tar.gz"
+  sha256 "3d31f2250a31636391474d92730c6e6219a4c9f5b282359f7e212858fbdb9822"
   license "Apache-2.0"
 
 
